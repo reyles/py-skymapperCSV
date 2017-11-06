12d8832910456d7c145cb7ec13c36d5aedd56c42
Rob A.J. Eyles <raje1@xroa-dt-20.star.le.ac.uk>
2017-11-06 14:19:57 +0000

    Tests done

--- Test.py
+++ Test.py
@@ -13,9 +13,43 @@ import csv
 
 sys.path.append('/home/r/raje1/Documents/')
 import requests
-from bs4 import BeautifulSoup  
+from bs4 import BeautifulSoup
+
+#Test user input
+waveband = str(input('Please choose your filter [B/V/R/u/g/r/i/z] = '))
+
+star_ra = []
+star_dec = []
+star_mag = []
+star_magerr = []
 
 with open('skymapperCatalogue.csv','rb') as cat:
-	reader = csv.reader(cat)
-	for row in reader:
-		print row
+	next(cat)
+	
+    	reader = csv.reader(cat, delimiter=',')
+
+    	for row in reader:
+		if waveband == 'r':
+			if row[66] != '':
+				star_mag.append(row[66])
+				star_magerr.append(row[67])
+				star_ra.append(row[2])
+				star_dec.append(row[3])
+		if waveband == 'i':
+			if row[70] != '':
+				star_mag.append(row[70])
+				star_magerr.append(row[71])
+				star_ra.append(row[2])
+				star_dec.append(row[3])
+		if waveband == 'z':
+			if row[74] != '':
+				star_mag.append(row[74])
+				star_magerr.append(row[75])
+				star_ra.append(row[2])
+				star_dec.append(row[3])
+
+	print star_mag
+	print star_magerr
+	print star_ra
+	print star_dec
+		
